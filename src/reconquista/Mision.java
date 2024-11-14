b7b2599e8ba3e8d7777f4e2dd256f5c8dd1b7c9c
Christian-Rojas-Rodriguez <christianrojas.datascience@gmail.com>
2024-11-14 00:48:18 -0300

    fix: Class Mision

--- src/reconquista/Mision.java
+++ src/reconquista/Mision.java
@@ -93,33 +93,34 @@ public class Mision {
 		return cantPueblos;
 	}
 
-	mision.mostrarMision();
-
-	Dijkstra dij = new Dijkstra(mision.getPueblos());
-	List<Integer> camino = dij.obtenerCaminoMasCorto(mision.getPuebloInicial(), mision.getPuebloFinal());
+	public void RealizarMision () {
+		Dijkstra dij = new Dijkstra(getPueblos());
+		List<Integer> camino = dij.obtenerCaminoMasCorto(getPuebloInicial(), getPuebloFinal());
 
 		System.out.println();
 		System.out.println("Camino mas corto");
 		System.out.println("----------------");
-		for (int i = 1 ; i < camino.size() ; i++) {
-		System.out.println((i) + " -> " + (camino.get(i)));
+		for (int i = 1; i < camino.size(); i++) {
+			System.out.println((i) + " -> " + (camino.get(i)));
+
+			Pueblo puebloPropio = getPueblo(0);
+			Ejercito propio = new Ejercito(puebloPropio.getHabitantes(), puebloPropio.getRaza());
+
+			Pueblo puebloActual = getPueblo(2);
+
+			switch (puebloActual.getRelacion()) {
+				case "aliado":
+					System.out.println("\nEstas en un pueblo aliado, tu ejercito descansa y se incorporan " + puebloActual.getHabitantes() / 2 + " de raza " + puebloActual.getRaza());
+					propio.descansarEjercito();
+					propio.incorporarEjercito(puebloActual.getHabitantes() / 2, puebloActual.getRaza());
+					break;
+				case "enemigo":
+					System.out.println("\nEstas en un pueblo enemigo, tu ejercito entra en combate con la tropas " + puebloActual.getRaza() + " con un ejercito de " + puebloActual.getHabitantes());
+					Ejercito enemigo = new Ejercito(puebloActual.getHabitantes(), puebloActual.getRaza());
+					Combate combate = new Combate(propio, enemigo);
+					combate.combate();
+					break;
+			}
+		}
 	}
-
-/*		Pueblo puebloPropio = mision.getPueblo(0);
-		Ejercito propio = new Ejercito(puebloPropio.getHabitantes(), puebloPropio.getRaza());
-
-		Pueblo puebloActual = mision.getPueblo(2);
-
-		switch (puebloActual.getRelacion()) {
-			case "aliado":
-				System.out.println("\nEstas en un pueblo aliado, tu ejercito descansa y se incorporan " + puebloActual.getHabitantes()/2 + " de raza " + puebloActual.getRaza());
-				propio.descansarEjercito();
-				propio.incorporarEjercito(puebloActual.getHabitantes()/2, puebloActual.getRaza());
-				break;
-			case "enemigo":
-				System.out.println("\nEstas en un pueblo enemigo, tu ejercito entra en combate con la tropas " + puebloActual.getRaza() + " con un ejercito de " + puebloActual.getHabitantes());
-				Ejercito enemigo = new Ejercito(puebloActual.getHabitantes(), puebloActual.getRaza());
-				combate(propio, enemigo);
-				break;
-		}*/
 }
